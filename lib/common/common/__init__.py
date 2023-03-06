cf752dc0b65ed2987f59f6a8ac9a957d0f262d45
freedumb2000 <freedumb2000@example.com>
2023-03-06 11:23:17 +0100

    ad example lib ("common") and project

---
+++ lib/common/common/__init__.py
@@ -0,0 +1,5 @@
+# from hello_world import hello_world
+
+
+def hello_world():
+    print("Hello World!")
---
+++ projects/frontend/app/app.py
@@ -0,0 +1,5 @@
+from common.hello_universe import hello_universe
+from common import hello_world
+# print(hello_world())
+hello_world()
+hello_universe()
