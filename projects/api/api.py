3f1e97d3facf742b9786d32694bfb3ea59a7db06
freedumb2000 <freedumb2000@example.com>
2023-03-08 17:25:13 +0100

    add return dummy data

--- projects/api/api.py
+++ projects/api/api.py
@@ -22,7 +22,7 @@ def root():
 
 
 @app.post("/uploader")
-async def create_upload_file(file: UploadFile = File(...), num_of_results=5):
+async def create_upload_file(file: UploadFile = File(...), data={'num_of_results': 5}):
 
     uploaded_image = Image.open(file.file)
 
@@ -30,7 +30,7 @@ async def create_upload_file(file: UploadFile = File(...), num_of_results=5):
 
     # -- Dummy data
     df = pd.read_csv('image_links.csv')
-    df_resized = df.head(num_of_results)
+    df_resized = df.head(int(data.get('num_of_results')))
     # replace NaN with empty string
     df_resized = df_resized.fillna('')
     result_dict = df_resized.to_dict('records')
