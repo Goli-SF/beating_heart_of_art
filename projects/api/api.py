f5378313c615106600684b57fb02db9baaa7eb03
freedumb2000 <freedumb2000@example.com>
2023-03-08 15:50:42 +0100

    add  poetry config; rename app file to api.py

--- projects/api/api.py
+++ projects/api/api.py
@@ -1,16 +1,16 @@
-from fastapi import FastAPI, File, UploadFile
+from fastapi import FastAPI
 from PIL import Image
 from io import BytesIO
 import pandas as pd
-#import shutil
+import shutil
 
 app = FastAPI()
 
 
 @app.get("/recommend")
 def dummy():
-    return{
-        'Message' : 'This page is supposed to return the N most similar images'
+    return {
+        'Message': 'This page is supposed to return the N most similar images'
     }
 
 
@@ -18,17 +18,18 @@ def dummy():
 def root():
     return {
         'Message': '❤️ Welcome to the beating ❤️ of art ❤️ '
-        }
+    }
 
 
 @app.post("/uploader/")
-async def create_upload_file(file: UploadFile = File(...), num_of_results = 5):
+async def create_upload_file(file: UploadFile = File(...), num_of_results=5):
     uploaded_image = Image.open(BytesIO(file.file))
-    resized_image = uploaded_image.resize((224,224)) #just to test if the Image.open works
+    # just to test if the Image.open works
+    resized_image = uploaded_image.resize((224, 224))
 
     df = pd.read_csv('/home/krishnaram/code/image_links.csv')
     return {'nearest_neighbours': df.tail(num_of_results).to_dict('records')}
-    #return output_dict #should return the list as written out below back to Gunther's streamliot
+    # return output_dict #should return the list as written out below back to Gunther's streamliot
 
 '''
 Response object back to Streamlit
