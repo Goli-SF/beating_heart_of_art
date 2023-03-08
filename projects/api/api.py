9d3fbfffafdf91b0acac48f70ff392509c8278b1
freedumb2000 <freedumb2000@example.com>
2023-03-08 16:19:33 +0100

    fix return data

--- projects/api/api.py
+++ projects/api/api.py
@@ -21,37 +21,19 @@ def root():
     }
 
 
-@app.post("/uploader/")
+@app.post("/uploader")
 async def create_upload_file(file: UploadFile = File(...), num_of_results=5):
-    uploaded_image = Image.open(BytesIO(file.file))
-    # just to test if the Image.open works
+
+    uploaded_image = Image.open(file.file)
+
     resized_image = uploaded_image.resize((224, 224))
 
-    df = pd.read_csv('/home/krishnaram/code/image_links.csv')
-    return {'nearest_neighbours': df.tail(num_of_results).to_dict('records')}
-    # return output_dict #should return the list as written out below back to Gunther's streamliot
+    # -- Dummy data
+    df = pd.read_csv('image_links.csv')
+    df_resized = df.head(num_of_results)
+    # replace NaN with empty string
+    df_resized = df_resized.fillna('')
+    result_dict = df_resized.to_dict('records')
 
-'''
-Response object back to Streamlit
-
-
-V2 - more information
-Let us return some dummy data for name, location, year for V1
-        'nearest_neighbors': [{link: 'link1', 'name' : 'name1' , 'location' : 'Paris', 'year' : 1755}
-            , {link: 'link2', 'name' : 'name2' , 'location' : 'London', 'year' : 1955}]
-
-        }
-'''
-
-'''
-Request object from Streamlit
-
-from fastapi import FastAPI, File, UploadFile
-import shutil
-@app.post("/uploader/")
-async def create_upload_file(file: UploadFile = File(...)):
-   with open("destination.png", "wb") as buffer:
-      shutil.copyfileobj(file.file, buffer)
-   return {"filename": file.filename}
-
-'''
+    result = {'nearest_neighbours': result_dict}
+    return result
