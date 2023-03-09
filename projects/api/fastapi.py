1d309737b9a1927cd68194b92b506ab3fc6fa8e0
Krishna Ram <krishna1986@gmail.com>
2023-03-09 10:02:57 +0100

    Updating fastapi file to pull image from streamlit

--- projects/api/fastapi.py
+++ projects/api/fastapi.py
@@ -1,8 +1,6 @@
 from fastapi import FastAPI, File, UploadFile
 from PIL import Image
-from io import BytesIO
 import pandas as pd
-#import shutil
 
 app = FastAPI()
 
@@ -23,11 +21,15 @@ def root():
 
 @app.post("/uploader/")
 async def create_upload_file(file: UploadFile = File(...), num_of_results = 5):
-    uploaded_image = Image.open(BytesIO(file.file))
+    uploaded_image = Image.open(file.file)
     resized_image = uploaded_image.resize((224,224)) #just to test if the Image.open works
+
+    #pass the image to Eric along with the n-neighbors
+
 
     df = pd.read_csv('/home/krishnaram/code/image_links.csv')
     return {'nearest_neighbours': df.tail(num_of_results).to_dict('records')}
+
     #return output_dict #should return the list as written out below back to Gunther's streamliot
 
 '''
@@ -40,17 +42,4 @@ Let us return some dummy data for name, location, year for V1
             , {link: 'link2', 'name' : 'name2' , 'location' : 'London', 'year' : 1955}]
 
         }
-'''
-
-'''
-Request object from Streamlit
-
-from fastapi import FastAPI, File, UploadFile
-import shutil
-@app.post("/uploader/")
-async def create_upload_file(file: UploadFile = File(...)):
-   with open("destination.png", "wb") as buffer:
-      shutil.copyfileobj(file.file, buffer)
-   return {"filename": file.filename}
-
 '''
