58c99007659983f3f7768b551c8234f312f6d627
Eric Macedo <eric.macedo@gmail.com>
2023-03-09 09:58:02 +0100

    added the main.py file

---
+++ projects/interface/main.py
@@ -0,0 +1,51 @@
+from tensorflow.keras.preprocessing.image import load_img
+from tensorflow.keras.preprocessing.image import img_to_array
+from tensorflow.keras.applications.vgg16 import preprocess_input
+# models
+from tensorflow.keras.applications.vgg16 import VGG16
+from tensorflow.keras.models import Model
+# neighbors and dimension reduction
+from sklearn.neighbors import NearestNeighbors
+from sklearn.decomposition import PCA
+# for everything else
+import os
+import numpy as np
+import matplotlib.pyplot as plt
+from random import randint
+import pandas as pd
+import pickle
+
+
+model = VGG16()
+model = Model(inputs = model.inputs, outputs = model.layers[-2].output)
+pca = pickle.load(open("<LOCATION_OF_PICKLE_FILE>","rb"))
+X = pickle.load(open("<LOCATION_OF_PICKLE_FILE>","rb"))
+
+def extract_features(image, model):
+    '''
+    Extract features of an image file using the selected model.
+    '''
+    # load the image as a 224x224 array
+    #img = load_img(file, target_size=(224,224))
+    # convert from 'PIL.Image.Image' to numpy array
+    img = np.array(image)
+    # reshape the data for the model reshape(num_of_samples, dim 1, dim 2, channels)
+    reshaped_img = img.reshape(1,224,224,3)
+    # prepare image for model
+    imgx = preprocess_input(reshaped_img)
+    # get the feature vector
+    features = model.predict(imgx, use_multiprocessing=True)
+    # pca of the features
+    target = pca.transform(features)
+
+    return target
+
+
+def find_neighbors(X, target, n_neighbors=20):
+    '''
+    Find the nearest neighbors of a target image.
+    '''
+    neigh = NearestNeighbors(n_neighbors=n_neighbors)
+    neigh.fit(X)
+    kneighbors = neigh.kneighbors(target.reshape(1,-1))[1].tolist()[0]
+    return kneighbors
