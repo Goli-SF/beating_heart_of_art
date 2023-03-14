bf7ab268b11cc7810d29a3e15f27c939f2132751
Golnaz Sarkar Farshi <golnaz.s.farshi@gmail.com>
2023-03-14 12:54:38 +0100

    added random image and change

--- projects/frontend/app/app.py
+++ projects/frontend/app/app.py
@@ -183,7 +183,7 @@ def main():
     with col1:
         st.title(APP_TITLE, )
     with col2:
-        st.image('beating-heart-copy.gif')
+        st.image('random_image.jpg')
     st.caption(APP_SUB_TITLE)
 
     # Radio with two upload options
